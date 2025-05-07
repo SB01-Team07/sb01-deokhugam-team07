72b44dfcb2e44bdaab78e43e4ad3b469e33e59b4
reflash407 <reflash407@naver.com>
2025-05-07 15:37:31 +0900

    feat: ReviewUpdateScheduler 클래스, runSyncJob 메서드 추가

---
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/scheduler/ReviewUpdateScheduler.java
@@ -0,0 +1,22 @@
+package com.part3.team07.sb01deokhugamteam07.scheduler;
+
+import com.part3.team07.sb01deokhugamteam07.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ReviewUpdateScheduler {
+
+    private final ReviewService reviewService;
+
+    @Scheduled(cron = "0 */30 * * * *")
+    public void runSyncJob() {
+        log.info("리뷰 카운트 동기화 시작");
+        reviewService.syncReviewCounts();
+        log.info("리뷰 카운트 동기화 완료");
+    }
+}
